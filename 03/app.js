159b3f7f28bbfe2b141df4ad784402fed49e1082
kamranikramofficial <kamranikram706@gmail.com>
2025-04-15 02:05:18 +0700

    add 03

---
+++ 03/app.js
@@ -0,0 +1,31 @@
+        const marginSlider = document.getElementById('marginSlider');
+        const opacitySlider = document.getElementById('opacitySlider');
+        
+        const marginValue = document.getElementById('marginValue');
+        const opacityValue = document.getElementById('opacityValue');
+        
+        const images = document.querySelectorAll('.stack-image');
+        
+        function updateMargin() {
+            const value = marginSlider.value;
+            marginValue.textContent = `${value}px`;
+            
+            images[0].style.top = `${value}px`;
+            images[0].style.left = `${value}px`;
+            
+        }
+        
+        function updateOpacity() {
+            const value = opacitySlider.value;
+            opacityValue.textContent = `${value}%`;
+            
+            images.forEach(image => {
+                image.style.opacity = value / 100;
+            });
+        }
+        
+        marginSlider.addEventListener('input', updateMargin);
+        opacitySlider.addEventListener('input', updateOpacity);
+        
+        updateMargin();
+        updateOpacity();
