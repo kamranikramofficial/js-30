0d0cdc09ca64d30cdef8bd61d1eb47140a555f6c
kamranikramofficial <kamranikramofficial@gmail.com>
2025-05-12 23:43:54 +0500

    add

---
+++ 16/app.js
@@ -0,0 +1,15 @@
+const glowText = document.getElementById('glowText');
+
+document.addEventListener('mousemove', (e) => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = (e.clientX / width - 0.5) * 100;
+  const y = (e.clientY / height - 0.5) * 100;
+
+  glowText.style.textShadow = `
+    ${x}px ${y}px 10px #00ffff,
+    ${-x}px ${-y}px 15px #ff00ff,
+    ${y}px ${-x}px 25px #ffeb3b,
+    ${-y}px ${x}px 40px #00ffe1
+  `;
+});
